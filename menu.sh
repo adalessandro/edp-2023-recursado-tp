8775fae99bd8a5f486041a55164e565c0a77a352
Rocio <rociopq4@gmail.com>
2023-11-17 20:35:42 -0300

    agregue opcion Salir

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 
 PS3='Elija> '
 echo "Hola, soy el menu del TP. Que quiere hacer?"
-select opcion in "Clasificar Archivos" "Monitorear Sistema" "Analizar Texto"
+select opcion in "Clasificar Archivos" "Monitorear Sistema" "Analizar Texto" "Salir"
 do
 [ -z $opcion ] && echo "Elegir opcion valida!" && continue
 [ $REPLY == 1 ] && echo "Eligio Clasificar Archivos" && continue
