a527957215dede575ff3e291ef38f1e970544289
Gon <longogonzalo.g@gmail.com>
2023-12-06 17:37:31 -0300

    Creacion contenedor usuario y persistencia de datos

--- 02-usuario/scripts/menu.sh
+++ 02-usuario/scripts/menu.sh
@@ -6,11 +6,11 @@ echo "Hola soy el menu del TP, que desea hacer?"
 select opcion in "Clasificar Archivos" "Monitorear Sistema" "Analizar Texto" "Salir"
 do
  [[ -z $opcion ]] && echo "Elegir opcion valida!" && continue
- [ $REPLY == 1 ] && echo bash clasificar.sh && continue
- [ $REPLY == 2 ] && echo bash monitorear.sh && continue
- [ $REPLY == 3 ] && echo bash analizar.sh && continue
+ [ $REPLY == 1 ] && bash clasificar.sh && continue
+ [ $REPLY == 2 ] && bash monitor.sh && continue
+ [ $REPLY == 3 ] && bash analizar.sh && continue
  [ $REPLY == 4 ] && echo "Eligio Salir" && break
- echo "Opcion elegida: " $opcion
+ echo "Opcion elegida: " $opcion #No se ve por el continue
 done
 
 exit 0
